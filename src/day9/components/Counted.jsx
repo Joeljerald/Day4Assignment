6e28adae6ab3043020d466acc7fc1765563da29d
Joeljerald <joelarul2004@gmail.com>
2026-06-23 00:51:56 +0530

    updated

--- src/day9/components/Counted.jsx
+++ src/day9/components/Counted.jsx
@@ -1,4 +1,4 @@
-const Count = ({ count }) => {
+const Counted = ({ count }) => {
   return (
     <div className="w-64 mx-auto mt-10 p-6 bg-white rounded-xl shadow-md text-center">
       <h1 className="text-3xl font-bold">Count : {count}</h1>
@@ -6,4 +6,4 @@ const Count = ({ count }) => {
   );
 };
 
-export default Count;
+export default Counted;
