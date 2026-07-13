cbbad181941360dc5d33192da2a3c502a32cbd8f
Joeljerald <joelarul2004@gmail.com>
2026-07-13 23:18:38 +0530

    day21

--- src/pages/Day21.jsx
+++ src/pages/Day21.jsx
@@ -1,16 +1,22 @@
+import Navbar from "../components/Navbar"
 import EmployeeMemo from "../day21/EmployeeMemo"
 import ProductMemo from "../day21/ProductMemo"
 import ShoppingMemo from "../day21/ShoppingMemo"
 import StudentMemo from "../day21/StudentMemo"
+import PageLayout from "../PageLayout"
 
 
 const Day21 = () => {
   return (
     <>
+    <PageLayout>
+    <Navbar/>
     <ProductMemo/>
     <ShoppingMemo/>
     <StudentMemo/>
     <EmployeeMemo/>
+    </PageLayout>
+    
     
     
     
